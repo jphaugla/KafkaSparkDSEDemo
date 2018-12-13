f82d18d4d51b952b00d25ab5675514e884c655e8
Jason Haugland <jasonhaugland@gmail.com>
2018-12-12 21:48:19 -0800

    fix python data generating producer

--- producer/src/main/python/generateData.py
+++ producer/src/main/python/generateData.py
@@ -12,27 +12,29 @@ producer = KafkaProducer(bootstrap_servers='node0:9092')
 #  keep this open until killed
 while 1:
 	#  open csv file holding sensors metadata 
-	filename = "producer/data/hive_sensorsClean1000.csv"
+	filename = "producer/data/hive_sensorsClean5.csv"
 	f = open(filename,'r') 
 	#  get current time
-	current_time = str(datetime.datetime.now().time() )
-	current_date = str(datetime.datetime.now().date() )
+	current_time = str(datetime.datetime.now() )
+        cntr = 0
+	edge_id = "Edge 1"
 	#   use the csv reader to parse the csv file
 	csv_f = csv.reader(f)
 	#    loop through all the sensors in the file
 	for nextSensor in csv_f:
 	#  generate random movement number
-		depth = random.randint(1, 100)
+		cntr += 1
+		if cntr > 1:
+			depth = random.randint(1, 100) + random.random()
 	#   generate random humidity set
-		value = random.randint(1, 100)
+			value = random.randint(1, 100) + random.random()
 	#   sensor serial number is first column in the csv
-		serial_number = nextSensor[0]
-                edge_id = "Edge 1"
+			serial_number = nextSensor[0]
 	#   send to the link
 	#	print 'this is serial %s' % (serial_number)
-		print_string =  '%s,%s %s,%s,%s,%s\n' % (edge_id, serial_number,value,current_time,depth,value)
-#		print (print_string)
-                producer.send("stream_ts", print_string)
-                producer.flush()
-	        time.sleep(10)
+			print_string =  '%s;%s;%s;%s;%s;%s\n' % (edge_id, serial_number,value,current_time,depth,value)
+			print (print_string)
+        		producer.send("stream_ts", print_string)
+        		producer.flush()
+	time.sleep(10)
 # close the client socket
